1429935f997cb422d538f455cffb4b21523f860f
Yannik195 <yannisim@gmail.com>
2022-01-09 13:31:07 +0100

    unique subdomain

--- routes/events.js
+++ routes/events.js
@@ -14,7 +14,7 @@ router.get("/", auth, async (req, res) => {
 })
 
 //Get one event
-router.get("/:subdomain", async (req, res) => {
+router.get("/subdomain/:subdomain", async (req, res) => {
     console.log("Get event")
     try {
         const event = await Event.findOne({ subdomain: req.params.subdomain })
@@ -27,6 +27,7 @@ router.get("/:subdomain", async (req, res) => {
 //Create new event
 router.post("/", auth, async (req, res) => {
     console.log("create event")
+    console.log(req.body)
     const event = new Event({
         name: req.body.name,
         subdomain: req.body.subdomain,
@@ -48,6 +49,19 @@ router.post("/", auth, async (req, res) => {
         res.send(savedEvent)
     } catch (err) {
         res.status(400).send(err)
+    }
+})
+
+//Check if subdomain is unique
+//used in "create event"
+
+router.get("/subdomainIsUnique", auth, async (req, res) => {
+    console.log("subdomain")
+    const event = await Event.findOne({ subdomain: req.body.subdomain })
+    if (!event) {
+        res.send("true")
+    } else {
+        res.send("false")
     }
 })
 
