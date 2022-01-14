4d3b44d6738df89115372574810d6244547f0b1d
Yannik195 <yannisim@gmail.com>
2022-01-14 21:30:05 +0100

    overviews

--- routes/attendencies.js
+++ routes/attendencies.js
@@ -12,14 +12,18 @@ router.get("/:eventId", async (req, res) => {
     }
 })
 
-//check in
+//check in / check out
 router.put("/checkin/:attendencieId", auth, async (req, res) => {
     console.log("Checkin: " + req.params.attendencieId)
     console.log(req.body)
     try {
         const attendencie = await Attendencie.findOne({ _id: req.params.attendencieId })
         attendencie.checkedIn = req.body.checkedIn
-        attendencie.checkedInTime = Date.now()
+        if (req.body.checkIn) {
+            attendencie.checkedInTime = Date.now()
+        } else {
+            attendencie.checkedInTime = null
+        }
         await attendencie.save()
         res.send(attendencie)
     } catch (err) {
