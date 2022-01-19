b6db12ab638d3e787c7dd106a89ef12f9ab06f98
Yannik195 <yannisim@gmail.com>
2022-01-20 00:05:42 +0100

    cookie

--- routes/auth.js
+++ routes/auth.js
@@ -26,7 +26,7 @@ router.post("/login", async (req, res) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.setHeader('Access-Control-Allow-Origin', 'https://einladung.app');
 
-    res.cookie("auth-token", token, { maxAge: 48 * 60 * 60 * 1000, domain: "einladung.app" }).send()
+    res.cookie("auth-token", token, { maxAge: 48 * 60 * 60 * 1000, domain: "einladung.app", httpOnly: false }).send()
 })
 
 router.post("/register", async (req, res) => {
